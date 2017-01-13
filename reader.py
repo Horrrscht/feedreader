607234b2195f183d5df45e9d7d89bb44ba6598d6
Birger Schulze <birger.schulze@mni.thm.de>
2017-01-13 04:46:10 +0100

    Changed feedcontent from window to pad

--- reader.py
+++ reader.py
@@ -3,28 +3,47 @@
 import curses
 from curses.textpad import rectangle
 
-lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+keyBindTexts = [
+    "> Switch to textfield",
+    "< Switch to feedlist",
+    "q Quit",
+    "r Reload selected feed"
+]
+
+feedText = """Spielregeln für die neue Medienwelt
+
+Stets Zugriff auf Nachrichten aus aller Welt haben, sich mit Freunden austauschen oder nachts einkaufen gehen - das Internet bietet unzählige Möglichkeiten. Doch es drohen auch Gefahren: so können Nutzer zu viele Informationen von sich preisgeben oder sich sogar in der Virtualität verlieren. Was sie benötigen, ist Medienkompetenz - zum Beispiel im Umgang mit sozialen Netzwerken. ARD.de zeigt, welche Anforderungen die neue Medienwelt an uns stellt.
+
+http://www.ard.de/home/ard/Medienkompetenz/76910/index.html
+"""
 
 feeds = ["Hello", "World", "Bla", "Blubb", "Wurst", "Kaese", "Quark"]
 feedListDims = (2, 1, 6, 25)  # y,x,height,width
 feedContentDims = (
-feedListDims[0] + feedListDims[2] + 3, 1, 10, 66)  # TODO: Set dimensions dynamically according to terminal size
+    feedListDims[0] + feedListDims[2] + 3, 1, 50, 66)  # TODO: Set dimensions dynamically according to terminal size
 
 
 def main(stdscr):
+    feedWindowHeight = 15
     selectedFeed = 0
     feedOffset = 0
     textLine = 0
     stdscr.addstr(0, 0, "Welcome to my super-awesome feedreader")
 
     feedListWin = curses.newwin(feedListDims[2], feedListDims[3], feedListDims[0], feedListDims[1])
-    feedContentWin = curses.newwin(feedContentDims[2], feedContentDims[3], feedContentDims[0], feedContentDims[1])
+    #feedContentWin = curses.newwin(feedContentDims[2], feedContentDims[3], feedContentDims[0], feedContentDims[1])
+    feedContentWin = curses.newpad(feedContentDims[2], feedContentDims[3])
     activeWin = feedContentWin
 
     rectangle(stdscr, feedListDims[0] - 1, feedListDims[1] - 1, feedListDims[0] + feedListDims[2],
               feedListDims[1] + feedListDims[3] + 1)
-    rectangle(stdscr, feedContentDims[0] - 1, feedContentDims[1] - 1, feedContentDims[0] + feedContentDims[2],
+    rectangle(stdscr, feedContentDims[0] - 1, feedContentDims[1] - 1, feedContentDims[0] + feedWindowHeight+1,
               feedContentDims[1] + feedContentDims[3] + 1)
+
+    stdscr.move(2, 28)
+    for line in range(0, len(keyBindTexts)):
+        stdscr.addstr(keyBindTexts[line])
+        stdscr.move(3 + line, 28)
 
     ch = ""
     while (ch != "q"):
@@ -38,20 +57,24 @@ def main(stdscr):
                 feedListWin.addnstr(feeds[feedOffset:][line], feedListDims[3])
 
         feedContentWin.move(0, 0)
-        feedContentWin.addnstr(lorem[textLine * feedContentDims[3]:],
-                               feedContentDims[2] * feedContentDims[3] - 1)  # TODO: Ist das so richtig?
-        # Der hat anscheinend probleme mit Zeilenumbrüchen
+        #feedContentWin.addnstr(feedText[textLine * feedContentDims[3]:],
+        #                       feedContentDims[2] * feedContentDims[3] - 1) # TODO: Ist das so richtig?
+                                                                            # TODO: Der hat anscheinend probleme mit Zeilenumbrüchen
+                                                                            # Das liegt daran, dass Zeilenumbrüche die Anzahl der Zeichen pro Textfeld runtersetzen
+        feedContentWin.addstr(feedText)
 
         feedListWin.refresh()
-        feedContentWin.refresh()
+        feedContentWin.refresh(textLine, 0, feedContentDims[0], feedContentDims[1], feedContentDims[0]+feedWindowHeight, feedContentDims[1]+feedContentDims[3])
         ch = stdscr.getkey()
 
         if ch == "KEY_LEFT" and activeWin == feedContentWin:
             activeWin = feedListWin
             feedListWin.move(0, 0)
+            curses.curs_set(0)
         elif ch == "KEY_RIGHT" and activeWin == feedListWin:
             activeWin = feedContentWin
             feedContentWin.move(0, 0)
+            curses.curs_set(2)
         elif ch == "KEY_UP" and activeWin == feedListWin and selectedFeed > 0:
             selectedFeed -= 1
             if feedOffset > 0:
@@ -62,8 +85,7 @@ def main(stdscr):
                 feedOffset += 1
         elif ch == "KEY_UP" and activeWin == feedContentWin and textLine > 0:
             textLine -= 1
-        elif ch == "KEY_DOWN" and activeWin == feedContentWin and textLine < len(lorem) / feedContentDims[3] - \
-                feedContentDims[2]:  # TODO: Anzahl Zeilen zählen
+        elif ch == "KEY_DOWN" and activeWin == feedContentWin:  # TODO: Anzahl Zeilen zählen
             textLine += 1
 
 
