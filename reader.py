5589642c6441388244a0247fdd2d3557facc4436
Birger Schulze <birger.schulze@mni.thm.de>
2017-01-13 01:21:29 +0100

    Scrolling in feedlist now works correctly

--- reader.py
+++ reader.py
@@ -1,4 +1,4 @@
- # -*- coding: utf8 -*-
+# -*- coding: utf8 -*-
 
 import curses
 from curses.textpad import rectangle
@@ -6,20 +6,25 @@ from curses.textpad import rectangle
 lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
 
 feeds = ["Hello", "World", "Bla", "Blubb", "Wurst", "Kaese", "Quark"]
-feedListDims = (2, 1, 6, 25) #y,x,height,width
-feedContentDims = (feedListDims[0]+feedListDims[2]+3, 1, 10, 66) #TODO: Set dimensions dynamically according to terminal size
+feedListDims = (2, 1, 6, 25)  # y,x,height,width
+feedContentDims = (
+feedListDims[0] + feedListDims[2] + 3, 1, 10, 66)  # TODO: Set dimensions dynamically according to terminal size
+
 
 def main(stdscr):
     selectedFeed = 0
+    feedOffset = 0
     textLine = 0
     stdscr.addstr(0, 0, "Welcome to my super-awesome feedreader")
 
-    feedListWin = curses.newwin(feedListDims[2] ,feedListDims[3], feedListDims[0], feedListDims[1])
-    feedContentWin = curses.newwin(feedContentDims[2] ,feedContentDims[3], feedContentDims[0], feedContentDims[1])
+    feedListWin = curses.newwin(feedListDims[2], feedListDims[3], feedListDims[0], feedListDims[1])
+    feedContentWin = curses.newwin(feedContentDims[2], feedContentDims[3], feedContentDims[0], feedContentDims[1])
     activeWin = feedContentWin
 
-    rectangle(stdscr, feedListDims[0]-1, feedListDims[1]-1, feedListDims[0]+feedListDims[2], feedListDims[1]+feedListDims[3]+1)
-    rectangle(stdscr, feedContentDims[0]-1, feedContentDims[1]-1, feedContentDims[0]+feedContentDims[2], feedContentDims[1]+feedContentDims[3]+1)
+    rectangle(stdscr, feedListDims[0] - 1, feedListDims[1] - 1, feedListDims[0] + feedListDims[2],
+              feedListDims[1] + feedListDims[3] + 1)
+    rectangle(stdscr, feedContentDims[0] - 1, feedContentDims[1] - 1, feedContentDims[0] + feedContentDims[2],
+              feedContentDims[1] + feedContentDims[3] + 1)
 
     ch = ""
     while (ch != "q"):
@@ -27,14 +32,15 @@ def main(stdscr):
         feedListWin.move(0, 0)
         for line in range(0, feedListDims[2] if len(feeds) > feedListDims[2] else len(feeds)):
             feedListWin.move(line, 0)
-            if line == selectedFeed:          
-                feedListWin.addnstr(feeds[0 if selectedFeed>=feedListDims[2] else selectedFeed - feedListDims[2]:][line], feedListDims[3], curses.A_STANDOUT)
+            if line + feedOffset == selectedFeed:
+                feedListWin.addnstr(feeds[feedOffset:][line], feedListDims[3], curses.A_STANDOUT)
             else:
-                feedListWin.addnstr(feeds[line], feedListDims[3])
+                feedListWin.addnstr(feeds[feedOffset:][line], feedListDims[3])
 
         feedContentWin.move(0, 0)
-        feedContentWin.addnstr(lorem[textLine*feedContentDims[3]:], feedContentDims[2]*feedContentDims[3]-1) #TODO: Ist das so richtig?
-                            #Der hat anscheinend probleme mit Zeilenumbrüchen
+        feedContentWin.addnstr(lorem[textLine * feedContentDims[3]:],
+                               feedContentDims[2] * feedContentDims[3] - 1)  # TODO: Ist das so richtig?
+        # Der hat anscheinend probleme mit Zeilenumbrüchen
 
         feedListWin.refresh()
         feedContentWin.refresh()
@@ -48,11 +54,17 @@ def main(stdscr):
             feedContentWin.move(0, 0)
         elif ch == "KEY_UP" and activeWin == feedListWin and selectedFeed > 0:
             selectedFeed -= 1
+            if feedOffset > 0:
+                feedOffset -= 1
         elif ch == "KEY_DOWN" and activeWin == feedListWin and selectedFeed < feedListDims[2]:
             selectedFeed += 1
+            if selectedFeed >= feedListDims[2]:
+                feedOffset += 1
         elif ch == "KEY_UP" and activeWin == feedContentWin and textLine > 0:
             textLine -= 1
-        elif ch == "KEY_DOWN" and activeWin == feedContentWin and textLine < len(lorem)/feedContentDims[3]-feedContentDims[2]: #TODO: Anzahl Zeilen zählen
+        elif ch == "KEY_DOWN" and activeWin == feedContentWin and textLine < len(lorem) / feedContentDims[3] - \
+                feedContentDims[2]:  # TODO: Anzahl Zeilen zählen
             textLine += 1
+
 
 curses.wrapper(main)
