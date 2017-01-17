e83f182685c639abbfbcf4a1e39459869a040104
Birger Schulze <birger.schulze@mni.thm.de>
2017-01-17 14:25:50 +0100

    Feedmanager now downloads feed if no file given

--- feedmanager.py
+++ feedmanager.py
@@ -1,19 +1,36 @@
 # -*- coding: utf8 -*-
 
 import json
+from downloader import *
+from rss2json import *
 
 def getFeedList():
     return json.load(open("feedlist.json"))["feeds"] #TODO: Error-Check
 
-
+def reloadFeed(name, url):
+    pass
 
 def getFeed(name):
-    source = json.load(open("feedlist.json"))["feeds"]
-    for feed in source:
+    feedlist = open("feedlist.json", "r")
+    source = json.load(feedlist)
+    feedlist.close()
+    for feed in source["feeds"]:
         if feed["name"] == name:
+            if feed["file"] == None:
+                filename = "{}.json".format(name)
+                xmlFile = "{}.xml".format(name)
+                downloadCurl(feed["url"], xmlFile)
+                file = open(filename, "w")
+                file.write(dict2json(rss2dict(xmlFile)))
+                file.close()
+                feed["file"] = filename
+                feedlist = open("feedlist.json", "w")
+                feedlist.write(dict2json(source))
+                feedlist.close()
+                return getFeed(name)
             return json.load(open(feed["file"]))
     return None #TODO: Error if feed not in feedlist
 
 #print(getFeedList())
 
-getFeed("ARD")
+print(getFeed("XKCD"))
