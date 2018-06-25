c7c6159b60111b10313c100db50f45974521286b
Birger Schulze <birger.schulze@mni.thm.de>
2018-06-26 01:22:53 +0200

    Replaced curl

--- feedmanager.py
+++ feedmanager.py
@@ -3,7 +3,7 @@
 # Author: Birger Schulze
 
 import json
-from downloader import downloadCurl
+from downloader import downloadCurl, download
 from rss2json import dict2json, rss2dict
 
 
@@ -17,15 +17,16 @@ def getFeedList():
 def reloadFeed(name, url, feedDict, sourceDict):
     filename = "{}.json".format(name)
     xmlFile = "{}.xml".format(name)
-    downloadCurl(url, xmlFile)
+    #downloadCurl(url, xmlFile)
+    xml = download(url)
     file = open(filename, "w")
-    file.write(dict2json(rss2dict(xmlFile)))
+    #file.write(dict2json(rss2dict(xmlFile)))
+    file.write(dict2json(rss2dict(xml)))
     file.close()
     feedDict["file"] = filename
     feedlist = open("feedlist.json", "w")
     feedlist.write(dict2json(sourceDict))
     feedlist.close()
-
 
 def getFeed(name, forceReload):
     feedlist = open("feedlist.json", "r")
@@ -39,6 +40,3 @@ def getFeed(name, forceReload):
             return json.load(open(feed["file"]))
     return None  # TODO: Error if feed not in feedlist
 
-# print(getFeedList())
-
-#getFeed("ARD")
